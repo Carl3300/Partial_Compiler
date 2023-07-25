609460285b081f71cd09cf61fa7d74021d8f33c1
Carl3300 <davenpra@mail.uc.edu>
2023-07-24 22:36:07 -0400

    major adjustment

--- code_parser.py
+++ code_parser.py
@@ -1,29 +1,94 @@
 # Token types
 TOKEN_KEYWORD = 'KEYWORD'
 TOKEN_IDENTIFIER = 'IDENTIFIER'
-TOKEN_OPERATOR = 'OPERATOR'
 TOKEN_LITERAL = 'LITERAL'
+TOKEN_EOF = 'EOF'
 
-curr = 0
+#TOKEN_OPERATORS
+TOKEN_PLUS = 'PLUS'
+TOKEN_MINUS = 'MINUS'
+TOKEN_MULTIPLY = 'MULTIPLY'
+TOKEN_DIVIDE = 'DIVIDE'
+TOKEN_MOD = 'MOD'
+TOKEN_LTHAN = 'LTHAN'
+TOKEN_RTHAN = 'RTHAN'
+TOKEN_NOT = 'NOT'
+TOKEN_BOR = 'BOR'
+TOKEN_XOR = 'XOR'
+TOKEN_BNOT = 'BNOT'
+TOKEN_ASSIGN = 'ASSIGN'
+TOKEN_TERNARY = 'TERNARY'
+TOKEN_COLON = 'COLON'
+TOKEN_COMMA = 'COMMA'
+TOKEN_BAND = 'BAND'
+TOKEN_LPAREN = 'LPAREN'
+TOKEN_RPAREN = 'RPAREN'
+TOKEN_LBRACKET = 'LBRACKET'
+TOKEN_RBRACKET = 'RBRACKET'
 
-def parse_expression(tokens):
-    global curr
-    left_operand = parse_term(tokens)
 
-    while curr < len(tokens) and tokens[curr][0] in (TOKEN_OPERATOR,):
-        operator = tokens[curr][1]
-        curr += 1
+# reference operators = [ '+', '-', '*', '/', '%', '<', '>', '!', '|', '^', '~', '=', '?', ':', ',', '*', '&', '(', ')', '{', '}']
+def parse(tokens):
+    def get_next_token():
+        if tokens:
+            return tokens.pop(0)
+        return None
 
-        right_operand = parse_term(tokens)
+    def parse_expr():
+        return parse_term()
 
-        if operator == '+':
-            left_operand += right_operand
-        elif operator == '-':
-            left_operand -= right_operand
-    return left_operand
+    def parse_term():
+        node = parse_factor()
 
-def parse_term(tokens):
-    global curr
-    term = tokens[curr][1]
-    curr += 1
-    return term
+        while current_token and current_token['type'] in (TOKEN_MULTIPLY, TOKEN_DIVIDE):
+            op = current_token
+            consume_token()
+            right = parse_factor()
+            node = {'type': AST_BINOP, 'operator': op, 'left': node, 'right': right}
+
+        return node
+
+    def parse_factor():
+        token = current_token
+        if token['type'] == TOKEN_INTEGER:
+            consume_token()
+            return {'type': AST_INTEGER, 'value': token['value']}
+        elif token['type'] == TOKEN_IDENTIFIER:
+            consume_token()
+            return {'type': AST_IDENTIFIER, 'name': token['value']}
+        elif token['type'] == TOKEN_LPAREN:
+            consume_token()
+            node = parse_expr()
+            if current_token['type'] == TOKEN_RPAREN:
+                consume_token()
+            else:
+                raise SyntaxError('Expected closing parenthesis')
+            return node
+        else:
+            raise SyntaxError('Invalid token: ' + token['type'])
+
+    def parse_assignment():
+        left = parse_factor()
+        if current_token['type'] == TOKEN_ASSIGN:
+            op = current_token
+            consume_token()
+            right = parse_expr()
+            return {'type': AST_ASSIGN, 'left': left, 'operator': op, 'right': right}
+        else:
+            raise SyntaxError('Expected assignment operator')
+
+    def consume_token():
+        nonlocal current_token
+        current_token = get_next_token()
+
+    # Start parsing
+    current_token = get_next_token()
+    ast = []
+
+    while current_token:
+        if current_token['type'] in (TOKEN_IDENTIFIER, TOKEN_INTEGER):
+            ast.append(parse_assignment())
+        else:
+            raise SyntaxError('Unexpected token: ' + current_token['type'])
+
+    return ast
--- tokenizer.py
+++ tokenizer.py
@@ -1,23 +1,46 @@
 # Token types
 TOKEN_KEYWORD = 'KEYWORD'
 TOKEN_IDENTIFIER = 'IDENTIFIER'
-TOKEN_OPERATOR = 'OPERATOR'
 TOKEN_LITERAL = 'LITERAL'
+TOKEN_EOF = 'EOF'
 
-# all keywords in C
+#TOKEN_OPERATORS
+TOKEN_PLUS = 'PLUS'
+TOKEN_MINUS = 'MINUS'
+TOKEN_MULTIPLY = 'MULTIPLY'
+TOKEN_DIVIDE = 'DIVIDE'
+TOKEN_MOD = 'MOD'
+TOKEN_LTHAN = 'LTHAN'
+TOKEN_RTHAN = 'RTHAN'
+TOKEN_NOT = 'NOT'
+TOKEN_BOR = 'BOR'
+TOKEN_XOR = 'XOR'
+TOKEN_BNOT = 'BNOT'
+TOKEN_ASSIGN = 'ASSIGN'
+TOKEN_TERNARY = 'TERNARY'
+TOKEN_COLON = 'COLON'
+TOKEN_COMMA = 'COMMA'
+TOKEN_BAND = 'BAND'
+TOKEN_LPAREN = 'LPAREN'
+TOKEN_RPAREN = 'RPAREN'
+TOKEN_LBRACKET = 'LBRACKET'
+TOKEN_RBRACKET = 'RBRACKET'
+
+# all keywords in C, maybe subcalssify as type, comparison, loops, return and other
 keywords = [ 'auto', 'break', 'case', 'char', 'const', 'continue', 'default', 'do', 'double', 'else', 'enum', 'extern',
              'float', 'for', 'goto', 'if', 'int', 'long', 'register', 'return', 'short', 'signed', 'sizeof', 'static', 
              'struct', 'switch', 'typedef', 'union', 'unsigned', 'void', 'volatile', 'while']
 
 # all operators in C
-operators = [ '+', '-', '*', '/', '%', '<', '>', '!', '|', '^', '~', '=', '?', ':', ',', '*', '&', '(', ')', '{', '}']
+operators = [ '+', '-', '*', '/', '%', '<', '>', '!', '|', '^', '~', '=', '?', ':', ',', '&', '(', ')', '{', '}']
 
 def tokenize(lines):
     tokens = []
-    realTokens = []
     isString = False
     word = ''
+    line_num = 0
     for line_number, line in enumerate(lines, 1):
+        line_num = line_number
         for char in line:
             if isString:
                 if char != '"':
@@ -39,19 +62,58 @@ def tokenize(lines):
                 if char.isspace():    
                     continue
                 if char in operators:
-                    tokens.append((TOKEN_OPERATOR, char, line_number))
+                    if char =='+':
+                        tokens.append((TOKEN_PLUS, char, line_number))
+                    elif char == '-':
+                        tokens.append((TOKEN_MINUS, char, line_number))
+                    elif char == '*':
+                        tokens.append((TOKEN_MULTIPLY, char, line_number))
+                    elif char == '/':
+                        tokens.append((TOKEN_DIVIDE, char, line_number))
+                    elif char == '%':
+                        tokens.append((TOKEN_MOD, char, line_number))
+                    elif char == '<':
+                        tokens.append((TOKEN_LTHAN, char, line_number))
+                    elif char == '>':
+                        tokens.append((TOKEN_RTHAN, char, line_number))
+                    elif char == '!':
+                        tokens.append((TOKEN_NOT, char, line_number))
+                    elif char == '|':
+                        tokens.append((TOKEN_BOR, char, line_number))
+                    elif char == '^':
+                        tokens.append((TOKEN_XOR, char, line_number))
+                    elif char == '~':
+                        tokens.append((TOKEN_BNOT, char, line_number)) 
+                    elif char == '=':
+                        tokens.append((TOKEN_ASSIGN, char, line_number))
+                    elif char == '?':
+                        tokens.append((TOKEN_TERNARY, char, line_number)) 
+                    elif char == ':':
+                        tokens.append((TOKEN_COLON, char, line_number))
+                    elif char == ',':
+                        tokens.append((TOKEN_COMMA, char, line_number)) 
+                    elif char == '&':
+                        tokens.append((TOKEN_BAND, char, line_number))
+                    elif char == '(':
+                        tokens.append((TOKEN_LPAREN, char, line_number)) 
+                    elif char == ')':
+                        tokens.append((TOKEN_RPAREN, char, line_number)) 
+                    elif char == '{':
+                        tokens.append((TOKEN_LBRACKET, char, line_number)) 
+                    elif char == '}':
+                        tokens.append((TOKEN_RBRACKET, char, line_number)) 
                     word = ''
                 if char == '"':
                     word += '"'
                     isString = True
-        # needed if there is word at end of line
-        if word:
+        if word: #word at the end of file
             if word in keywords:
                 tokens.append((TOKEN_KEYWORD, word, line_number))
             else:
                 tokens.append((TOKEN_IDENTIFIER, word, line_number))
             word = ''
-    # cant think of better way to filter without a bunch of bools
+    realTokens = []
+    tokens.append((TOKEN_EOF, '', line_num)) 
     for token in tokens:
         if token[0] == TOKEN_IDENTIFIER:
             if token[1][0].isnumeric() or token[1][0] == "-":
